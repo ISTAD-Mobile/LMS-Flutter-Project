fd46f3918fc60b35b2b84fee855e531453713ef1
leang bunleap <leangbunleap168@gmail.com>
2025-01-15 16:10:43 +0700

    It neww

--- lib/view/widgets/public_screen_widgets/home/it_news/It_news_card.dart
+++ lib/view/widgets/public_screen_widgets/home/it_news/It_news_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lms_mobile/data/color/color_screen.dart';
 import 'package:lms_mobile/model/jobvacancy.dart';
+import 'package:lms_mobile/view/widgets/public_screen_widgets/home/it_news/it_news_detail.dart';
 
 class ItNewsCard extends StatelessWidget {
   ItNewsCard(this.jobvacancy);
@@ -11,82 +12,93 @@ class ItNewsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String formattedDate = DateFormat('dd MMM yyyy').format(jobvacancy.publishedAt);
-    return Container(
-      width: 310,
-      margin: const EdgeInsets.only(right: 5),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          width: 0.5,
-          color: Colors.grey.shade400,
+
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the job details page
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ItNewsDetail(),
+          ),
+        );
+      },
+      child: Container(
+        width: 310,
+        margin: const EdgeInsets.only(right: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 0.5,
+            color: Colors.grey.shade400,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            ),
+          ],
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.1),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              child: Image.network(
+                '${jobvacancy.thumbnail}',
+                width: double.infinity,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
             ),
-            child: Image.network(
-              '${jobvacancy.thumbnail}',
-              width: double.infinity,
-              height: 150,  // Fixed height for the image
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            height: 50,  // Fixed height for the content
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        jobvacancy.contentType!.type.toString(),
-                        style: const TextStyle(
-                          fontSize: 12,
-                          color: AppColors.primaryColor,
+            Container(
+              padding: const EdgeInsets.all(18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          jobvacancy.contentType!.type.toString(),
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: AppColors.primaryColor,
+                          ),
                         ),
-                      ),
-                      Text(
-                        formattedDate,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          color: AppColors.primaryColor,
+                        Text(
+                          formattedDate,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: AppColors.primaryColor,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  jobvacancy.title,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+                  const SizedBox(height: 10),
+                  Text(
+                    jobvacancy.title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/view/widgets/public_screen_widgets/home/it_news/it_news_detail.dart
+++ lib/view/widgets/public_screen_widgets/home/it_news/it_news_detail.dart
@@ -1,224 +1,219 @@
-// import 'package:flutter/material.dart';
-// import 'package:lms_mobile/data/color/color_screen.dart';
-// import 'package:lms_mobile/model/jobvacancy.dart';
-//
-// class ItNewsDetail extends StatelessWidget {
-//   final JobVacancy jobVacancy;
-//
-//   const ItNewsDetail({super.key, required this.jobVacancy});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: AppColors.defaultWhiteColor,
-//       appBar: AppBar(
-//         backgroundColor: Colors.white,
-//         elevation: 0,
-//         leading: IconButton(
-//           icon: const Icon(Icons.arrow_back, color: AppColors.defaultGrayColor),
-//           onPressed: () => Navigator.pop(context),
+import 'package:flutter/material.dart';
+import 'package:lms_mobile/data/color/color_screen.dart';
+
+class ItNewsDetail extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.defaultWhiteColor,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: AppColors.defaultGrayColor),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title Section
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Title',
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                    color: AppColors.primaryColor,
+                ),
+              ),
+            ),
+            const SizedBox(height: 2),
+            // Main Image
+            ClipRRect(
+              child: Image.network(
+                'thumbnail',
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            // Date and Category Row
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.calendar_month,
+                        size: 16,
+                        color: AppColors.primaryColor,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                       'publishedAt',
+                        style: const TextStyle(
+                          color: AppColors.primaryColor,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                    Row(
+                      children: [
+                        const Icon(Icons.folder,
+                          size: 16,
+                          color: AppColors.primaryColor,
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 4,
+                          ),
+                          child: Text(
+                            'contentType.type',
+                            style: const TextStyle(
+                              color: AppColors.primaryColor,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Description
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'editorContent',
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailRow(IconData icon, String label, String value) {
+    return Row(
+      children: [
+        Icon(icon, size: 20, color: AppColors.primaryColor),
+        const SizedBox(width: 8),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(width: 8),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.black87,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+// Text(
+//   news.description,
+//   style: const TextStyle(
+//     fontSize: 16,
+//     color: AppColors.defaultGrayColor,
+//     height: 1.5,
+//   ),
+// ),
+// const SizedBox(height:30),
+// // Course Details
+// Container(
+//   padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+//   decoration: BoxDecoration(
+//     borderRadius: BorderRadius.circular(8),
+//   ),
+//   child: Column(
+//     crossAxisAlignment: CrossAxisAlignment.start,
+//     children: [
+//       ClipRRect(
+//         borderRadius: const BorderRadius.vertical(
+//           top: Radius.circular(8),
+//           bottom: Radius.circular(8),
 //         ),
-//         // title: Text(
-//         //   jobvacancyModel.title,
-//         //   style: const TextStyle(
-//         //     fontSize: 18,
-//         //     color: AppColors.primaryColor
-//         //   ),
-//         // ),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             // Title Section
-//             Padding(
-//               padding: const EdgeInsets.all(20),
-//               child: Text(
-//                 jobVacancy.title,
-//                 style: const TextStyle(
-//                   fontSize: 22,
-//                   fontWeight: FontWeight.w500,
-//                     color: AppColors.primaryColor,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 2),
-//             // Main Image
-//             ClipRRect(
-//               child: Image.network(
-//                 jobVacancy.thumbnail,
-//                 width: double.infinity,
-//                 fit: BoxFit.cover,
-//               ),
-//             ),
-//
-//             // Date and Category Row
-//             Padding(
-//               padding: const EdgeInsets.all(16),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 children: [
-//                   Row(
-//                     children: [
-//                       const Icon(Icons.calendar_month,
-//                         size: 16,
-//                         color: AppColors.primaryColor,
-//                       ),
-//                       const SizedBox(width: 8),
-//                       Text(
-//                         jobVacancy.publishedAt,
-//                         style: const TextStyle(
-//                           color: AppColors.primaryColor,
-//                           fontSize: 14,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                   const SizedBox(width: 16),
-//                     Row(
-//                       children: [
-//                         const Icon(Icons.folder,
-//                           size: 16,
-//                           color: AppColors.primaryColor,
-//                         ),
-//                         const SizedBox(width: 8),
-//                         Container(
-//                           padding: const EdgeInsets.symmetric(
-//                             vertical: 4,
-//                           ),
-//                           child: Text(
-//                             jobVacancy.contentType.type,
-//                             style: const TextStyle(
-//                               color: AppColors.primaryColor,
-//                               fontSize: 14,
-//                             ),
-//                           ),
-//                         ),
-//                       ],
-//                     )
-//                 ],
-//               ),
-//             ),
-//             const SizedBox(height: 16),
-//             // Description
-//             Padding(
-//               padding: const EdgeInsets.all(16),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(
-//                     jobVacancy.editorContent,
-//                     style: const TextStyle(
-//                       fontSize: 22,
-//                       fontWeight: FontWeight.w500,
-//                       color: AppColors.primaryColor,
-//                     ),
-//                   ),
-//                   const SizedBox(height: 8),
-//                   // Text(
-//                   //   news.description,
-//                   //   style: const TextStyle(
-//                   //     fontSize: 16,
-//                   //     color: AppColors.defaultGrayColor,
-//                   //     height: 1.5,
-//                   //   ),
-//                   // ),
-//                   // const SizedBox(height:30),
-//                   // // Course Details
-//                   // Container(
-//                   //   padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-//                   //   decoration: BoxDecoration(
-//                   //     borderRadius: BorderRadius.circular(8),
-//                   //   ),
-//                   //   child: Column(
-//                   //     crossAxisAlignment: CrossAxisAlignment.start,
-//                   //     children: [
-//                   //       ClipRRect(
-//                   //         borderRadius: const BorderRadius.vertical(
-//                   //           top: Radius.circular(8),
-//                   //           bottom: Radius.circular(8),
-//                   //         ),
-//                   //         child: Image.network(
-//                   //           news.imageUrl1,
-//                   //           width: double.infinity,
-//                   //           fit: BoxFit.cover,
-//                   //         ),
-//                   //       ),
-//                   //       const SizedBox(height: 25,),
-//                   //       ClipRRect(
-//                   //         borderRadius: const BorderRadius.vertical(
-//                   //           top: Radius.circular(8),
-//                   //           bottom: Radius.circular(8),
-//                   //         ),
-//                   //         child: Image.network(
-//                   //           news.imageUrl2,
-//                   //           width: double.infinity,
-//                   //           fit: BoxFit.cover,
-//                   //         ),
-//                   //       ),
-//                   //       const SizedBox(height: 30,),
-//                   //       Text(
-//                   //         news.title,
-//                   //         style: const TextStyle(
-//                   //           fontSize: 18,
-//                   //           fontWeight: FontWeight.w500,
-//                   //           color: AppColors.primaryColor,
-//                   //         ),
-//                   //       ),
-//                   //       Text(
-//                   //         news.description,
-//                   //         style: const TextStyle(
-//                   //           fontSize: 16,
-//                   //           color: AppColors.defaultGrayColor,
-//                   //           height: 1.5,
-//                   //         ),
-//                   //       ),
-//                   //       const SizedBox(height: 20,),
-//                   //       ClipRRect(
-//                   //         borderRadius: const BorderRadius.vertical(
-//                   //           top: Radius.circular(8),
-//                   //           bottom: Radius.circular(8),
-//                   //         ),
-//                   //         child: Image.network(
-//                   //           news.imageUrl3,
-//                   //           width: double.infinity,
-//                   //           fit: BoxFit.cover,
-//                   //         ),
-//                   //       ),
-//                   //       const SizedBox(height: 25,)
-//                   //     ],
-//                   //   ),
-//                   // ),
-//                 ],
-//               ),
-//             ),
-//           ],
+//         child: Image.network(
+//           news.imageUrl1,
+//           width: double.infinity,
+//           fit: BoxFit.cover,
 //         ),
 //       ),
-//     );
-//   }
-//
-//   Widget _buildDetailRow(IconData icon, String label, String value) {
-//     return Row(
-//       children: [
-//         Icon(icon, size: 20, color: AppColors.primaryColor),
-//         const SizedBox(width: 8),
-//         Text(
-//           label,
-//           style: const TextStyle(
-//             fontSize: 14,
-//             fontWeight: FontWeight.bold,
-//           ),
+//       const SizedBox(height: 25,),
+//       ClipRRect(
+//         borderRadius: const BorderRadius.vertical(
+//           top: Radius.circular(8),
+//           bottom: Radius.circular(8),
 //         ),
-//         const SizedBox(width: 8),
-//         Text(
-//           value,
-//           style: const TextStyle(
-//             fontSize: 14,
-//             color: Colors.black87,
-//           ),
+//         child: Image.network(
+//           news.imageUrl2,
+//           width: double.infinity,
+//           fit: BoxFit.cover,
 //         ),
-//       ],
-//     );
-//   }
-// }
+//       ),
+//       const SizedBox(height: 30,),
+//       Text(
+//         news.title,
+//         style: const TextStyle(
+//           fontSize: 18,
+//           fontWeight: FontWeight.w500,
+//           color: AppColors.primaryColor,
+//         ),
+//       ),
+//       Text(
+//         news.description,
+//         style: const TextStyle(
+//           fontSize: 16,
+//           color: AppColors.defaultGrayColor,
+//           height: 1.5,
+//         ),
+//       ),
+//       const SizedBox(height: 20,),
+//       ClipRRect(
+//         borderRadius: const BorderRadius.vertical(
+//           top: Radius.circular(8),
+//           bottom: Radius.circular(8),
+//         ),
+//         child: Image.network(
+//           news.imageUrl3,
+//           width: double.infinity,
+//           fit: BoxFit.cover,
+//         ),
+//       ),
+//       const SizedBox(height: 25,)
+//     ],
+//   ),
+// ),
