6b60d511953d2bb5fff699e0e10925c8afc8fee5
Nhoem Tevy <tevymomnh193@gmail.com>
2025-02-12 16:45:08 +0700

    push

--- lib/view/widgets/public_screen_widgets/home/video_background.dart
+++ lib/view/widgets/public_screen_widgets/home/video_background.dart
@@ -17,7 +17,8 @@ class _VideoBackgroundState extends State<VideoBackground> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.network(
-        'https://static.vecteezy.com/system/browse_category/video/140/Cityscapes.mp4')
+        // 'https://static.vecteezy.com/system/browse_category/video/140/Cityscapes.mp4')
+      'https://rr2---sn-npoxu-2ois.googlevideo.com/videoplayback?expire=1739357029&ei=BSesZ-SbLtHUg8UPuqjkgQY&ip=136.228.158.126&id=o-AKHs_iTwgnvVnYwjnqNoie8lL8fOuoUIFUdsU878YBVB&itag=18&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&met=1739335429%2C&mh=JD&mm=31%2C29&mn=sn-npoxu-2ois%2Csn-npoe7nlz&ms=au%2Crdu&mv=m&mvi=2&pl=24&rms=au%2Cau&pcm2=yes&initcwndbps=1205000&bui=AUWDL3zGDgy3jfQgVksBDRasMTtm5s3_Gc5i2-p2Hez8qV3JPK59TRdwRfMKmX2uktNHhIrVmsxpj5cm&spc=RjZbSd4NzuCaxFm_2kzdZD7ofOePLYi5c21knnWhUmGOX-sLsouF0bBDwxrXWLAU_g&vprv=1&svpuc=1&mime=video%2Fmp4&ns=CRnDg4jcuiV_admMqTGNuXoQ&rqh=1&gir=yes&clen=1704251&ratebypass=yes&dur=30.696&lmt=1730387521026597&mt=1739335021&fvip=5&fexp=51326932%2C51331020&c=MWEB&sefc=1&txp=5309224&n=AnQINoy9v2EZ7w&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cpcm2%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AJfQdSswRAIgIscLipWBctEKJr2IMl5ulZ2YL6Zv0RrNu8sCJnLk7Z8CIH3UqQ_XdigrOIZPRWMZXG1NuH_7UxXvEdsEVorsFzfu&lsparams=met%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Crms%2Cinitcwndbps&lsig=AGluJ3MwRQIhAOF9RseNyJKCQMtr5b1Ex30rUGmUNELQlhjPgpYOppjbAiByv-KB6PVzYCKYtGEmpE4E4ZuiKWRNEupagTm34xpbqw%3D%3D')
       ..initialize().then((_) {
         _controller.setLooping(true);
         _controller.play();
